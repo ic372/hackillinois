3fb9cb5a35552e331b0b0558ad093c5eed3a88ef
ic372 <49081018+ic372@users.noreply.github.com>
2020-08-08 19:22:33 -0700

    Added test study groups, working on search bar

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,62 +1,154 @@
 import React from "react";
-import { Typography, Paper, Avatar, Button } from "@material-ui/core";
-import VerifiedUserOutlined from "@material-ui/icons/VerifiedUserOutlined";
-import withStyles from "@material-ui/core/styles/withStyles";
+import { Button, Typography } from "@material-ui/core";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import firebase from "../Fire";
 import { withRouter } from "react-router-dom";
 
 const styles = (theme) => ({
-  main: {
-    width: "auto",
-    display: "block", // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto",
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${
-      theme.spacing.unit * 3
-    }px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
   submit: {
     marginTop: theme.spacing.unit * 3,
   },
 });
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 1000,
+    marginTop: 15,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const SearchBarStyle = {
+  border: "solid",
+  borderRadius: "2px",
+  position: "relative",
+  left: "10vh",
+  height: "10vh",
+  width: "100vh",
+  marginTop: "5vh",
+  marginBottom: "10vh",
+};
+
 function Dashboard(props) {
   const { classes } = props;
-
-  if (!firebase.getCurrentUsername()) {
-    // not logged in
-    alert("Please login first");
-    props.history.replace("/login");
-    return null;
-  }
+  const cardClasses = useStyles();
 
   return (
-    <main className={classes.main}>
-      <Paper className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <VerifiedUserOutlined />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Hello {firebase.getCurrentUsername()}
-        </Typography>
+    <div>
+      <div className="searchBar">
+        <input
+          type="text"
+          placeholder="Enter item to be searched"
+          style={SearchBarStyle}
+          onChange={(e) => this.searchSpace(e)}
+        />
+      </div>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Card className={cardClasses.root}>
+            <CardContent>
+              <Typography
+                className={cardClasses.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                Subject 1
+              </Typography>
+              <Typography variant="h5" component="h2">
+                Class name 1
+              </Typography>
+              <Typography className={cardClasses.pos} color="textSecondary">
+                Prof name 1
+              </Typography>
+              <Typography variant="body2" component="p">
+                Study group description
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" variant="contained" color="primary">
+                Join Group
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card className={cardClasses.root}>
+            <CardContent>
+              <Typography
+                className={cardClasses.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                Subject 2
+              </Typography>
+              <Typography variant="h5" component="h2">
+                Class name 2
+              </Typography>
+              <Typography className={cardClasses.pos} color="textSecondary">
+                Prof name 2
+              </Typography>
+              <Typography variant="body2" component="p">
+                Study group description
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" variant="contained" color="primary">
+                Join Group
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card className={cardClasses.root}>
+            <CardContent>
+              <Typography
+                className={cardClasses.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                Subject 3
+              </Typography>
+              <Typography variant="h5" component="h2">
+                Class name 3
+              </Typography>
+              <Typography className={cardClasses.pos} color="textSecondary">
+                Prof name 3
+              </Typography>
+              <Typography variant="body2" component="p">
+                Study group description
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" variant="contained" color="primary">
+                Join Group
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
         <Button
           type="submit"
-          fullWidth
+          size="small"
           variant="contained"
           color="secondary"
           onClick={logout}
@@ -64,8 +156,8 @@ function Dashboard(props) {
         >
           Logout
         </Button>
-      </Paper>
-    </main>
+      </Grid>
+    </div>
   );
 
   async function logout() {
