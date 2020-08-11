8cbdce1f7dd6282bc43a9e919010b3f88285f964
Catelen Wu <cw3223@columbia.edu>
2020-08-11 10:45:20 -0400

    notes page

--- src/components/Notes2/index.js
+++ src/components/Notes2/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import TodoItems from "./TodoItems";
 import NavBar from "../NavBar";
 
 import "./TodoList.css";
@@ -37,46 +36,53 @@ class ToDoList2 extends Component {
   };
   render() {
     return (
-      <div className="App">
-        <h1>Notes</h1>
-        <textarea
-          onChange={event => this.setState({ currentNote: event.target.value })}
-          value={this.state.currentNote}
-          className="input"
-          placeholder="Notes"
-        />
-        <br/>
-        <button className="button" onClick={this.addNote}>
-          Submit
-        </button>
-        {this.state.notes.map((note, index) => (
-          <div className="notes" key={index}>
-            {this.state.noteEditing === null ||
-            this.state.noteEditing !== index ? (
-              <div className="note">
-                <div className="note-content">
-                  <div className="note-text">{note}</div>
-                  <button onClick={() => this.setNoteEditing(index)}>
-                    Edit
-                  </button>
-                </div>
-                <button onClick={() => this.deleteNote(index)}>Delete</button>
-              </div>
-            ) : (
-              <div className="note">
-                <div className="note-content">
-                  <input
-                  type="text"
-                  value={this.state.currentEdit}
-                    onChange={event => this.editNote(event)}
-                  />
-                  <button onClick={() => this.submitEdit(index)}>Done</button>
-                </div>
-                <button onClick={() => this.deleteNote(index)}>Delete</button>
-              </div>
-          )}
+      <div className="NotesPage">
+        <NavBar />
+        <div className="heading">
+          <h2>Your Notes</h2>
         </div>
-        ))}
+        <div className="todoListMain">
+          <div class="header">
+            <textarea
+              onChange={event => this.setState({ currentNote: event.target.value })}
+              value={this.state.currentNote}
+              className="input"
+              placeholder="Notes"
+            />
+            <br/>
+            <button className="button" onClick={this.addNote}>
+              Submit
+            </button>
+          </div>
+          {this.state.notes.map((note, index) => (
+            <div className="notes" key={index}>
+              {this.state.noteEditing === null ||
+              this.state.noteEditing !== index ? (
+                <div className="note">
+                  <div className="note-content">
+                    <div className="note-text">{note}</div>
+                    <button onClick={() => this.setNoteEditing(index)}>
+                      Edit
+                    </button>
+                  </div>
+                  <button onClick={() => this.deleteNote(index)}>Delete</button>
+                </div>
+              ) : (
+                <div className="note">
+                  <div className="note-content">
+                    <input
+                    type="text"
+                    value={this.state.currentEdit}
+                      onChange={event => this.editNote(event)}
+                    />
+                    <button onClick={() => this.submitEdit(index)}>Done</button>
+                  </div>
+                  <button onClick={() => this.deleteNote(index)}>Delete</button>
+                </div>
+            )}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
