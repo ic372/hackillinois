31979ee4c99a8734f9518d15cb87a3ae899a2da6
Catelen Wu <cw3223@columbia.edu>
2020-08-13 10:37:27 -0400

    updated profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import firebase from "../Fire";
 import NavBar from "../NavBar";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import "./profile.css";
+import 'firebase/auth';
 
-const Profile = () => {
+class Profile extends Component {
+  delete() {
+    var user = firebase.auth().currentUser;
+
+    user.delete().then(function() {
+      // User deleted.
+    }).catch(function(error) {  
+      // An error happened.
+    });
+  }
+
+  render() {
   return (
-    <div className="ProfilePage">
-      <NavBar />
-      <div className="WelcomeMessage">
-        <h2>Welcome {firebase.getCurrentUsername()}!</h2>
+      <div className="ProfilePage">
+        <NavBar />
+        <div className="WelcomeMessage">
+          <h2>Welcome {firebase.getCurrentUsername()}!</h2>
+        </div>
+        <MailOutlineIcon />
+        <div className="ContactInfo">
+          <h6>Your email: {firebase.getCurrentUserEmail()}</h6>
+        </div>
+        <button id="delete-button" onClick={this.delete}>
+        DELETE YOUR ACCOUNT
+        </button>
       </div>
-      <MailOutlineIcon />
-      <div className="ContactInfo">
-        <h6>Your email: {firebase.getCurrentUserEmail()}</h6>
-      </div>
-    </div>
-  );
-};
+    );
+  };
+}
 export default Profile;
